f315c4d4bd67424c1893089e8f88ba64cef429aa
manelfp24 <manuelmunozfp24@ibf.cat>
2026-05-26 18:18:59 +0200

    Animaciones Mbappe, Ester y Xabi

--- Assets/Scripts/MbappeController.cs
+++ Assets/Scripts/MbappeController.cs
@@ -10,14 +10,44 @@ public class MbappeController : MonoBehaviour
 
     // variable para registrar si hay movimiento del personaje
     public Vector2 input;
-    
+
+    private Animator animator;
+
+    // 1. NUEVO: Variable para guardar el componente visual del Sprite
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+
+        // 2. NUEVO: Buscamos el SpriteRenderer del personaje al iniciar el juego
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     //Función para updatear la posición del personaje
     private void Update()
     {
+        // 1. NUEVO/MODIFICADO: Registramos la pulsación al vuelo para el espejo
+        // Usamos Input.GetAxisRaw para saber hacia dónde quiere mirar AL INSTANTE
+        float direccionHorizontal = Input.GetAxisRaw("Horizontal");
+
+        if (direccionHorizontal < 0) 
+        {
+            spriteRenderer.flipX = true;  // Si pulsa izquierda, activamos espejo (mira a la izquierda)
+        }
+        else if (direccionHorizontal > 0) 
+        {
+            spriteRenderer.flipX = false; // Si pulsa derecha, quitamos espejo (mira a la derecha)
+        }
         if (!isMoving)
         {
             input.x = Input.GetAxisRaw("Horizontal");
             input.y = Input.GetAxisRaw("Vertical");
+
+            Debug.Log("This is input.x" + input.x);
+            Debug.Log("This is input.y" + input.y);
+
+            
 
             //Debug para ver si detecta que pulso las letras
             if (input != Vector2.zero) Debug.Log("Estoy pulsando una tecla: " + input);
@@ -28,6 +58,19 @@ public class MbappeController : MonoBehaviour
             //Vector2.zero es para indicar que la posición en (0, 0)
             if (input != Vector2.zero)
             {
+                // 3. NUEVO: Control del efecto espejo según la dirección
+                if (input.x < 0) 
+                {
+                    spriteRenderer.flipX = true;  // Activa el espejo (mira a la izquierda)
+                }
+                else if (input.x > 0) 
+                {
+                    spriteRenderer.flipX = false; // Desactiva el espejo (mira original a la derecha)
+                }
+
+                animator.SetFloat("moveX", input.x);
+                animator.SetFloat("moveY", input.y);
+
                 var targetPos = transform.position;
                 //*0.1f es para reducir la distancia de movimiento al pulsar una tecla de movimiento
                 targetPos.x += input.x * 0.1f; 
@@ -36,6 +79,8 @@ public class MbappeController : MonoBehaviour
                 StartCoroutine(Move(targetPos));
             }
         }
+
+        animator.SetBool("isMoving", isMoving); 
     }
 
     //Función de movimiento
